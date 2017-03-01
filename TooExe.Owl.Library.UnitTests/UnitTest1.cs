0d10d2fc1ab73d48e4d1dafd5e3115848cfcf3eb
Marcin Kotecki <marcin.kotecki@hotmail.com>
2017-03-02 00:17:04 +0100

    Create method  ToWordList and unit test

--- TooExe.Owl.Library.UnitTests/UnitTest1.cs
+++ TooExe.Owl.Library.UnitTests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TooExe.Owl.Library.UnitTests
@@ -9,9 +10,17 @@ namespace TooExe.Owl.Library.UnitTests
         [TestMethod]
         public void TestMethod1()
         {
-            var result = new Class1().ExampleMethod();
-
-            Assert.AreEqual("Ok",result);
+            var input = "This is my example.";
+            var expected = new List<string> { "This", "is", "my","example." } ;
+           
+           var actual = input.ToWordList() as List<string>;
+           
+                Assert.AreEqual(expected.Count, actual.Count);
+                for (var i = 0; i < expected.Count; i++)
+                {
+                    Assert.AreEqual(expected[i], actual[i]);
+                }
+            
         }
     }
 }
---
+++ TooExe.Owl.Library/StringUtility.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TooExe.Owl.Library
+{
+    public static class StringUtility
+    {
+        public static IEnumerable<string> ToWordList(this string text)
+        {
+            var endOfWord = ' ';
+            var chars = text.ToCharArray();
+            var result = new List<string>();
+            var sb = new StringBuilder();
+            var isNewWord = false;
+            foreach (var c in chars)
+            {
+                if (c == endOfWord)
+                {
+                    if (sb.Length > 0)
+                    {
+                        result.Add(sb.ToString());
+                        sb.Clear();
+                    }
+                    isNewWord = true;
+                    continue;
+                }
+
+                sb.Append(c);
+            }
+            //if last sight isn't space
+            if (sb.Length > 0)
+            {
+                result.Add(sb.ToString());
+                sb.Clear();
+            }
+            return result;
+
+        }
+    }
+}
