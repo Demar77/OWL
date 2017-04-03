db9f8caebadcc18eb87e8f96dbac79df8b583634
Admin <marcin.kotecki@tooexe.pl>
2017-04-03 14:51:40 +0200

    Migration first step

---
+++ src/TooExe.Owl.Mvc/Data/IApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TooExe.Owl.Mvc.Data
+{
+    internal interface IApplicationDbContext
+    {
+        DbSet<EnglishWord> EnglishWords { get; set; }
+    }
+
+    public class EnglishWord
+    {
+        [Key]
+        public string Id { get; set; }
+
+        public string Word { get; set; }
+        public string FileName { get; set; }
+    }
+}
---
+++ src/TooExe.Owl.Mvc/Data/Migrations/20170403125022_Test.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TooExe.Owl.Mvc.Data.Migrations
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EnglishWords",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    FileName = table.Column<string>(nullable: true),
+                    Word = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnglishWords", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnglishWords");
+        }
+    }
+}
